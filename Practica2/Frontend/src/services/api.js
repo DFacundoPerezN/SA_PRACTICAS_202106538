ee76a6a5264677ddbcd3ca17aa9faa0dd4813c61
DFaxx <3607834730101@ingenieria.usac.edu.gt>
2026-02-23 18:58:41 -0600

    Dockerized: backend

--- Practica2/Frontend/src/services/api.js
+++ Practica2/Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Configura aquí la URL de tu backend
-const API_BASE_URL = 'http://127.0.0.1:8080'
+const API_BASE_URL = 'http://20.49.4.20:8080'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
